15481d4a6cbf26105b5c742e924ba2db90aedadd
Vyacheslav <parselian.study@yandex.ru>
2022-07-16 23:09:19 +0300

    make js script more flexible

--- dev/assets/js/main.js
+++ dev/assets/js/main.js
@@ -1,63 +1,63 @@
-'use-strict';
+'use-strict'
 
-/*const returnDistanceAnalysis = (amount = 0, probability = 0, rise = 0, amountOfDeals = 0) => {
-  let failedTrades = 100 - probability,
-    numberPercentage = amountOfDeals / 100,
-    numberOfSuccessfulTransactions = numberPercentage * failedTrades,
-    numberOfFailedTransactions = numberPercentage * failedTrades,
-    distanceOfSuccessfulTransactions = amount * numberOfSuccessfulTransactions * (rise - 1),
-    distanceOfFailedTrades = amount * numberOfFailedTransactions,
-    distance = distanceOfSuccessfulTransactions - distanceOfFailedTrades
-
-  return distance
-}*/
-
-const returnDistanceAnalysis = (amount = 0,probability = 0,rise = 0,percentage = 0) => {
-  let failedTrades = 100 - probability,
-    numberOfSuccessfulTransactions = percentage * failedTrades,
-    numberOfFailedTransactions = percentage * failedTrades,
-    distanceOfSuccessfulTransactions = amount * numberOfSuccessfulTransactions * (rise - 1),
-    distanceOfFailedTrades = amount * numberOfFailedTransactions,
-    distance = distanceOfSuccessfulTransactions - distanceOfFailedTrades
-
-  return distance
-}
-
-const validateInputs = (input) => {
-  const inputID = input.getAttribute('id')
-
-  switch (true) {
-    case inputID === 'amount' && input.value >= 1:
-      return true
-    case inputID === 'rise' && input.value >= 0.01:
-      return true
-    case inputID === 'probability' && (input.value >= 0.05 && input.value <= 99.95):
-      return true
-    case inputID === 'percentage' && (input.value >= 0.01 && input.value <= 100):
-      return true
-    default:
-      return false
-  }
-}
-
-const calculateDistanceProfit = () => {
+const initDistanceProfitWidget = () => {
   const form = document.getElementById('distance-form'),
-    amountInput = document.getElementById('amount'),
-    riseInput = document.getElementById('rise'),
-    probability = document.getElementById('probability'),
-    percentage = document.getElementById('percentage'),
     profitInput = document.getElementById('profit')
 
-  form.addEventListener('focusout', (e) => {
-    if (e.target.matches('input') && validateInputs(e.target)) {
-      e.target.closest('.input__wrap').classList.remove('input__wrap_error')
-      profitInput.value = parseFloat(returnDistanceAnalysis(amountInput.value,probability.value, riseInput.value, percentage.value))
-    } else {
-      e.target.closest('.input__wrap').classList.add('input__wrap_error')
+  const getDistanceAnalysis = (data) => {
+    if (!data) {
+      return 'Your profit'
     }
+    let failedTrades = 100 - data['probability'],
+      numberOfSuccessfulTransactions = data['percentage'] * failedTrades,
+      numberOfFailedTransactions = data['percentage'] * failedTrades,
+      distanceOfSuccessfulTransactions = data['amount'] * numberOfSuccessfulTransactions * (data['rise'] - 1),
+      distanceOfFailedTrades = data['amount'] * numberOfFailedTransactions,
+      distance = distanceOfSuccessfulTransactions - distanceOfFailedTrades
+
+    return distance
+  }
+
+  const getInputsData = (inputs) => {
+    let data = {}
+
+    for (let key in inputs) {
+      const input = inputs[key],
+        val = parseFloat(input.value)
+
+      if (!isNaN(val) && val >= input.getAttribute('min') || val <= input.getAttribute('max')) {
+        input.closest('.input__wrap').classList.remove('input__wrap_error')
+        data[input.getAttribute('id')] = val
+        delete inputs[key]
+      } else {
+        input.closest('.input__wrap').classList.add('input__wrap_error')
+      }
+    }
+
+    if (Object.keys(inputs).length === 0) {
+      return data
+    } else {
+      return false
+    }
+  }
+
+  const showProfit = (profit) => {
+    profitInput.value = profit
+    if (parseFloat(profit) < 0) {
+      profitInput.closest('.input__wrap').classList.add('input__wrap_negative')
+    } else {
+      profitInput.closest('.input__wrap').classList.remove('input__wrap_negative')
+    }
+  }
+
+  form.addEventListener('focusout', (e) => {
+    let inputs = {...form.querySelectorAll('.widget-distance__form-vars input')},
+      profit = getDistanceAnalysis(getInputsData(inputs))
+
+    showProfit(profit)
   })
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  calculateDistanceProfit()
+  initDistanceProfitWidget()
 })
